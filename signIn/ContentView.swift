7ab2ea0d952548622e62902bb87dd062bff1ace3
Pavlo Semenchenko <imagemakerspa@icloud.com>
2023-11-01 21:35:08 +0200

    add sing in with phone

--- signIn/ContentView.swift
+++ signIn/ContentView.swift
@@ -6,6 +6,15 @@
 //
 
 import SwiftUI
+import FirebaseAuth
+import FirebaseAuthCombineSwift
+
+// need add Push Notification in Capabilities
+//        + Background modes
+//        + Remote notifications
+
+// firebase - cloud messaging !!! add apple key`s *
+// sertifacation- keys- turn Aple push notification + sign in with apple
 
 struct ContentView: View {
     var body: some View {
@@ -15,10 +24,43 @@ struct ContentView: View {
                 .foregroundStyle(.tint)
             Text("Hello, world!")
         }
+        .onAppear {
+            AuthService().authWithPhoneNumber(phone: "+380688880168")
+        }
         .padding()
     }
 }
 
 #Preview {
     ContentView()
+}
+
+class AuthService {
+    func authWithPhoneNumber(phone: String = "+380688880168") {
+        PhoneAuthProvider.provider().verifyPhoneNumber(phone, uiDelegate: nil) { verificationID, error in
+            print(verificationID)
+            print(error)
+            
+            guard let verificationID = verificationID else {
+                // show error to user
+                return
+            }
+            // next screen
+            self.signInWithPhone(verificationID: verificationID, verificationCode: "123456")
+            
+        }
+        // ContentView(verificationID: verificationID) - переход на второй экран
+    }
+    
+    func signInWithPhone(verificationID: String, verificationCode: String) {
+        let credential = PhoneAuthProvider.provider().credential(
+            withVerificationID: verificationID,
+            verificationCode: verificationCode
+        )
+        
+        Auth.auth().signIn(with: credential) { result, error in
+            print(result?.user.uid)
+            print(result?.additionalUserInfo?.isNewUser)
+        }
+    }
 }
--- signIn/signInApp.swift
+++ signIn/signInApp.swift
@@ -6,12 +6,46 @@
 //
 
 import SwiftUI
+import FirebaseCore
+import FirebaseAuth
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+  func application(_ application: UIApplication,
+                   didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+    FirebaseApp.configure()
+    return true
+  }
+    // for phone signIn - регистрация на получение пуш уведомлений
+    func application(_ application: UIApplication, didRegisterForRemoteNotificationWithDeviceToken deviceToken: Data) {
+        Auth.auth().setAPNSToken(deviceToken, type: .sandbox)
+    }
+    // for phone signIn - проверка нотификейшн от файрбейса
+    func application(_ application: UIApplication, didReceiveRemoteNotification notification: [AnyHashable : Any], fetchCompletionHandler completionHandler: @escaping (UIBackgroundFetchResult) -> Void) {
+      if Auth.auth().canHandleNotification(notification) {
+        completionHandler(.noData)
+        return
+      }
+    }
+    // for phone signIn - открытие по СХЕМЕ
+    func application(_ application: UIApplication, open url: URL, options: [UIApplication.OpenURLOptionsKey : Any]) -> Bool {
+      if Auth.auth().canHandle(url) {
+        return true
+      }
+      return false
+    }
+    
+}
 
 @main
-struct signInApp: App {
-    var body: some Scene {
-        WindowGroup {
-            ContentView()
-        }
+struct YourApp: App {
+  // register app delegate for Firebase setup
+  @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+
+  var body: some Scene {
+    WindowGroup {
+      NavigationView {
+        ContentView()
+      }
     }
+  }
 }
