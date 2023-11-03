864201c85d74f7f51938f34c83333bdd697f2ba4
Pavlo Semenchenko <imagemakerspa@icloud.com>
2023-11-03 22:01:46 +0200

    UI

---
+++ signIn/HomeScreen.swift
@@ -0,0 +1,18 @@
+//
+//  HomeScreen.swift
+//  signIn
+//
+//  Created by Pavel Semenchenko on 03.11.2023.
+//
+
+import SwiftUI
+
+struct HomeScreen: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    HomeScreen()
+}
---
+++ signIn/SplashScreen.swift
@@ -0,0 +1,18 @@
+//
+//  SplashScreen.swift
+//  signIn
+//
+//  Created by Pavel Semenchenko on 03.11.2023.
+//
+
+import SwiftUI
+
+struct SplashScreen: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SplashScreen()
+}
