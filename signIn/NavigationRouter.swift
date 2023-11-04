70534c8e4425a44e1e8e0a262489a6eddda5abe1
Pavlo Semenchenko <imagemakerspa@icloud.com>
2023-11-04 23:15:31 +0200

    begin add navigation

---
+++ signIn/NavigationRouter.swift
@@ -0,0 +1,18 @@
+//
+//  NavigationRouter.swift
+//  signIn
+//
+//  Created by Pavel Semenchenko on 04.11.2023.
+//
+
+import SwiftUI
+
+struct NavigationRouter: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    NavigationRouter()
+}
---
+++ signIn/SingInWithEmailScreen.swift
@@ -0,0 +1,18 @@
+//
+//  SingInWithEmailScreen.swift
+//  signIn
+//
+//  Created by Pavel Semenchenko on 04.11.2023.
+//
+
+import SwiftUI
+
+struct SingInWithEmailScreen: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SingInWithEmailScreen()
+}
