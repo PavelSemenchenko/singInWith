168cc26bf54aabc97f9c36322daa76410d91dbd1
Pavlo Semenchenko <imagemakerspa@icloud.com>
2023-11-05 00:19:02 +0200

    try setup email login

--- signIn/SingInWithEmailScreen.swift
+++ signIn/SingInWithEmailScreen.swift
@@ -24,6 +24,25 @@ struct SingInWithEmailScreen: View {
                     .frame(width: 100, height: 100)
                     .padding()
                 
+                EditField(valid: loginVM.isEmailCorrect, placeholder: "Email", text: $loginVM.email)
+                    .submitLabel(.next)
+                    .onSubmit {
+                        //внимание на другое поле
+                        //focusOnPassword()
+                        
+                    }
+                    .keyboardType(.emailAddress)
+                
+                PasswordField(valid: loginVM.isPaswordCorrect, placeholder: "Password", text: $loginVM.password)
+                    .submitLabel(.go)
+                    .onSubmit {
+                        Task {
+                            await loginVM.signIn()
+                            navigationVM.pushScreen(route: .home)
+                        }
+                    }
+                    //.focused($password)
+                /*
                 TextField("Email", text: $loginVM.email)
                     .textFieldStyle(RoundedBorderTextFieldStyle())
                     .padding()
@@ -31,7 +50,7 @@ struct SingInWithEmailScreen: View {
                 SecureField("Пароль", text: $loginVM.password)
                     .textFieldStyle(RoundedBorderTextFieldStyle())
                     .padding()
-                
+                */
                 Button(action: {
                     Task {
                         await loginVM.signIn()
@@ -49,8 +68,17 @@ struct SingInWithEmailScreen: View {
                         .background(Color.blue)
                         .foregroundColor(.white)
                         .cornerRadius(10)
+                    
                 }
                 .padding()
+                
+                MainButton(text: "Sign In", enabled: loginVM.canLogin, busy: loginVM.busy) {
+                    Task {
+                        await loginVM.signIn()
+                        // open TODOs
+                        navigationVM.pushScreen(route: .home)
+                    }
+                }
                 
                 NavigationLink("Забыли пароль?", destination: ResetPasswordScreen())
             }
@@ -139,7 +167,76 @@ class SingInWithEmailScreenVM: ObservableObject {
     }
 }
 
+// BUTTON SETUP
+fileprivate struct MainButtonStyle: ButtonStyle {
+    
+    let color: Color
+    
+    func makeBody(configuration: Configuration) -> some View {
+        configuration.label
+            .padding()
+            .background(color)
+            .foregroundColor(.white)
+            .clipShape(Capsule())
+    }
+}
+
+struct MainButton: View {
+    let text: String
+    let enabled: Bool
+    let busy: Bool
+    let action: () -> Void
+    
+    private var color: Color {
+        var color: Color = enabled ? .blue : .red
+        if busy {
+            color = .orange
+        }
+        return color
+    }
+    
+    var body: some View {
+        Button(text, action: action)
+            .buttonStyle(MainButtonStyle(color: color))
+            .disabled(!enabled || busy)
+    }
+}
+struct PasswordField: View {
+    var valid: Bool
+    var placeholder: String
+    @Binding var text: String
+    private var backgroundColor: Color {
+        valid ? .white : .red
+    }
+    
+    var body: some View {
+        SecureField(placeholder, text: $text)
+            .border(backgroundColor)
+            .cornerRadius(5)
+            .textFieldStyle(.roundedBorder)
+            .padding(EdgeInsets(top: 8, leading: 36, bottom: 8, trailing: 36))
+    }
+}
+struct EditField: View {
+    var valid: Bool
+    var placeholder: String
+    var text: Binding<String>
+    private var backgroundColor: Color {
+        valid ? .white : .red
+    }
+    
+    var body: some View {
+        TextField(placeholder, text: text)
+            .border(backgroundColor)
+            .cornerRadius(5)
+            .textFieldStyle(.roundedBorder)
+            //.background(backgroundColor)
+            .padding(EdgeInsets(top: 8, leading: 36, bottom: 8, trailing: 36))
+    }
+}
 
 #Preview {
     SingInWithEmailScreen()
+        .environmentObject(NavigationRouter())
+        .environmentObject(SingInWithEmailScreenVM())
 }
