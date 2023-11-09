074fbbe179716766414da524b60c2eff73c314b8
Pavlo Semenchenko <imagemakerspa@icloud.com>
2023-11-09 23:28:53 +0200

    ui

--- signIn/SignUpScreen.swift
+++ signIn/SignUpScreen.swift
@@ -9,14 +9,45 @@ import SwiftUI
 
 struct SignUpScreen: View {
     @EnvironmentObject private var navigationVM: NavigationRouter
-    @State private var userName = ""
+    @State private var userFirstName = ""
+    @State private var userLastName = ""
     
     var body: some View {
         VStack {
-            Text("Hello, new user")
-            TextField("Enter your name", text: $userName)
+            Text("Hello, new user").font(.title)
+            //add upload image
+            TextField("Enter your first name", text: $userFirstName)
                 .textFieldStyle(RoundedBorderTextFieldStyle())
+                .autocapitalization(.words)
                 .padding()
+            TextField("Enter your last name", text: $userLastName)
+                .textFieldStyle(RoundedBorderTextFieldStyle())
+                .autocapitalization(.words)
+                .padding()
+            
+            Button("Save Profile") {
+                guard !userFirstName.isEmpty, !userLastName.isEmpty else { return }
+                /*
+                // Save user profile to Firestore
+                FirestoreService.shared
+                    .createUser(uid: 
+                    authViewModel.user?.uid ?? "",
+                        userFirstName: userFirstName,
+                        userLastName: userLastName)
+                */
+                
+                // Navigate to the home screen
+                navigationVM.pushScreen(route: .home)
+            }
+            .disabled(userFirstName.isEmpty)
+            .padding()
+            .cornerRadius(8)
+            /*
+            Button(action: {
+                
+            }, label: {
+                Text("Done")
+            })*/
         }
     }
 }
