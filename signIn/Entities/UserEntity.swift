e81440e169d5171196a9a194bdf420215925265a
Pavlo Semenchenko <imagemakerspa@icloud.com>
2023-11-21 23:21:28 +0200

    try add user name

---
+++ signIn/Entities/UserEntity.swift
@@ -0,0 +1,18 @@
+//
+//  UserEntity.swift
+//  signIn
+//
+//  Created by Pavel Semenchenko on 21.11.2023.
+//
+
+import SwiftUI
+
+struct UserEntity: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    UserEntity()
+}
---
+++ signIn/Entities/UserRepository.swift
@@ -0,0 +1,8 @@
+//
+//  UserRepository.swift
+//  signIn
+//
+//  Created by Pavel Semenchenko on 21.11.2023.
+//
+
+import Foundation
